51fb692aa05811d7dd643bf095f6959757053979
igorMHJKS <igor_nunes13@icloud.com>
2022-09-22 22:59:03 -0300

    add and testing get route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req, res) => {
+
+    res.send("Hello Word")
+})
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
