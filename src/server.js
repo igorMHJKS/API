cd867cde9962f5b30777f91b00fcbc5e750969fa
igorMHJKS <igor_nunes13@icloud.com>
2022-09-23 22:12:13 -0300

    Add req.query nodemon

--- src/server.js
+++ src/server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 
 
-app.get("/", (req, res) => {
+app.get("/message/:id/:user", (request, response) => {
 
-    res.send("Hello Word")
+
+    const {id, user} = request.params;
+    response.send(`
+    Mensagem ID: ${id}.
+    Para o cliente: ${user}.
+    `)
+});
+
+app.get("/users", (request , response) =>{
+    const {page, limit} = request.query;
+
+    response.send(`Página: ${page}. Mostrar ${limit}`)
+    
 })
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
