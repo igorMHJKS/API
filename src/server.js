1f27deca823990d0c8b3b5c457e42403aec53672
igorMHJKS <igor_nunes13@icloud.com>
2022-09-23 23:21:50 -0300

    organizing project structure

--- src/server.js
+++ src/server.js
@@ -1,22 +1,11 @@
 const express = require("express");
+
+const routes= require("./routes")
+
 const app = express();
+app.use(express.json());
 
+app.use(routes);
 
-app.get("/message/:id/:user", (request, response) => {
-
-
-    const {id, user} = request.params;
-    response.send(`
-    Mensagem ID: ${id}.
-    Para o cliente: ${user}.
-    `)
-});
-
-app.get("/users", (request , response) =>{
-    const {page, limit} = request.query;
-
-    response.send(`Página: ${page}. Mostrar ${limit}`)
-    
-})
 const port = 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
